0cded07eebdedb53c450b9a208e97dba4442a193
abekolin <albertog4taz28@gmail.com>
2025-03-09 05:56:43 +0700

    add files

--- View Layouts - 5/app/Config/Routes.php
+++ View Layouts - 5/app/Config/Routes.php
@@ -6,6 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-$routes->get('/', [\App\Controllers\Home::class, 'index']);
-$routes->get('/Pages', [\App\Controllers\Pages::class, 'index']);
+$routes->get('/', [\App\Controllers\Pages::class, 'index']);
 $routes->get('/Pages/about', [\App\Controllers\Pages::class, 'about']);
