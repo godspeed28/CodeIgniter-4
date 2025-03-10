85189de3bc9e022c3eb62966962b999c57b4d314
abekolin <albertog4taz28@gmail.com>
2025-03-11 01:25:15 +0700

    error

---
+++ Upload File - 11/app/Controllers/Books.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\BooksModel;
+
+class Books extends BaseController
+{
+    protected $booksModel;
+    public function __construct()
+    {
+        $this->booksModel = new BooksModel();
+    }
+    public function index(): string
+    {
+        // $books = $this->booksModel->findAll();
+        $data = [
+            'title' => 'Books | PWL',
+            'books' => $this->booksModel->getBook()
+        ];
+
+        // // cara konek db tanpa model
+        // $db = \Config\Database::connect();
+        // $books = $db->query("SELECT * FROM books");
+        // foreach ($books->getResultArray() as $row) {
+        //     dd($row);
+        // }
+
+        // $booksModel = new \App\Models\BooksModel();
+
+        return view('books/index', $data);
+    }
+    public function detail($slug)
+    {
+        $data = [
+            'title' => 'Detail Book',
+            'books' => $this->booksModel->getBook($slug)
+        ];
+
+        if (empty($data['books'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul komik tidak ditemukan.');
+        }
+
+        return view('books/detail', $data);
+    }
+    public function create()
+    {
+        $data = [
+            'title' => 'Form Insert Data',
+            'validation' => session('validation')
+        ];
+
+        return view('books/create', $data);
+    }
+    public function save()
+    {
+        // validasi input
+        $rules = [
+            'judul' => [
+                'rules' => 'required|is_unique[books.judul]',
+                'errors' => [
+                    'required' => '{field} buku harus diisi.',
+                    'is_unique' => '{field} buku sudah terdaftar.'
+                ]
+            ],
+            'genre' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} buku harus diisi.'
+                ]
+            ],
+            'penulis' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} buku harus diisi.'
+                ]
+            ],
+            'tahun_terbit' => [
+                'label' => 'tahun terbit',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} buku harus diisi.'
+                ]
+            ],
+            // 'sampul' => 'uploaded[sampul]'
+        ];
+
+        if (!$this->validate($rules)) {
+            // dd($this->validation->getErrors());
+            return redirect()->back()->withInput()->with('validation', $this->validation);
+        }
+        // return redirect()->to('/Books/create')->withInput()->with('validation', $validation);
+        // return redirect()->to('/Books/create')->withInput();
+
+
+        $fileSampul = $this->request->getFile('sampul');
+
+        if ($fileSampul->isValid() && !$fileSampul->hasMoved()) {
+            $sampulName = $fileSampul->getRandomName();
+            $fileSampul->move('img', $sampulName);
+        } else {
+            $sampulName = 'hobbit.jpg'; // Jika tidak ada file, gunakan default
+        }
+
+
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+        // dd($this->request->getVar());
+        $this->booksModel->save([
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'genre' => $this->request->getVar('genre'),
+            'penulis' => $this->request->getVar('penulis'),
+            'tahun_terbit' => $this->request->getVar('tahun_terbit'),
+            'sampul' => $sampulName
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil ditambahkan.');
+
+        return redirect()->to('/Books');
+    }
+    public function delete($id)
+    {
+        $this->booksModel->delete($id);
+        session()->setFlashdata('pesan', 'Data berhasil dihapus.');
+        return redirect()->to('/Books');
+    }
+    public function edit($slug)
+    {
+        $data = [
+            'title' => 'Form Edit Data',
+            'validation' => session()->get('validation'),
+            'books' => $this->booksModel->getBook($slug)
+        ];
+
+        return view('books/edit', $data);
+    }
+    public function update($id)
+    {
+        // cek judul
+        $old_books = $this->booksModel->getBook($this->request->getVar('slug'));
+        if ($old_books['judul'] == $this->request->getVar('judul')) {
+            $rule_judul = 'required';
+        } else {
+            $rule_judul = 'required|is_unique[books.judul]';
+        }
+        // validasi input
+        if (
+            !$this->validate([
+                'judul' => [
+                    'rules' => $rule_judul,
+                    'errors' => [
+                        'required' => '{field} buku harus diisi.',
+                        'is_unique' => '{field} buku sudah terdaftar.'
+                    ]
+                ],
+                'genre' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => '{field} buku harus diisi.'
+                    ]
+                ],
+                'penulis' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => '{field} buku harus diisi.'
+                    ]
+                ],
+                'tahun_terbit' => [
+                    'label' => 'tahun terbit',
+                    'rules' => 'required',
+                    'errors' => [
+                        'required' => '{field} buku harus diisi.'
+                    ]
+                ]
+            ])
+        ) {
+            // $validation = \Config\Services::validation();
+            return redirect()->to('/Books/edit/' . $this->request->getVar('slug'))->withInput()->with('validation', $this->validation);
+        }
+        $slug = url_title($this->request->getVar('judul'), '-', true);
+        // dd($this->request->getVar());
+        $this->booksModel->save([
+            'id' => $id,
+            'judul' => $this->request->getVar('judul'),
+            'slug' => $slug,
+            'genre' => $this->request->getVar('genre'),
+            'penulis' => $this->request->getVar('penulis'),
+            'tahun_terbit' => $this->request->getVar('tahun_terbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ]);
+
+        session()->setFlashdata('pesan', 'Data berhasil diubah.');
+
+        return redirect()->to('/Books');
+    }
+}
---
+++ Upload File - 11/app/Views/books/create.php
@@ -0,0 +1,84 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h3 class="my-3">Form Insert Book</h3>
+            <form action="/Books/save" method="post" enctype="multipart/form-data">
+                <?= csrf_field(); ?>
+                <div class="mb-3 row">
+                    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                    <div class="col-sm-10">
+                        <input type="text"
+                            class="form-control <?= (isset($validation) && $validation->hasError('judul')) ? 'is-invalid' : ''; ?>"
+                            id="judul" name="judul" value="<?= old('judul'); ?>" autofocus>
+                        <div class="invalid-feedback">
+                            <?php if (isset($validation)): ?>
+                                <?= $validation->getError('judul'); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="genre" class="col-sm-2 col-form-label">Genre</label>
+                    <div class="col-sm-10">
+                        <input type="text"
+                            class="form-control <?= (isset($validation) && $validation->hasError('genre')) ? 'is-invalid' : ''; ?>"
+                            id="genre" name="genre" value="<?= old('genre'); ?>">
+                        <div class="invalid-feedback">
+                            <?php if (isset($validation)): ?>
+                                <?= $validation->getError('genre'); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
+                    <div class="col-sm-10">
+                        <input type="text"
+                            class="form-control <?= (isset($validation) && $validation->hasError('penulis')) ? 'is-invalid' : ''; ?>"
+                            id="penulis" name="penulis" value="<?= old('penulis'); ?>">
+                        <div class="invalid-feedback">
+                            <?php if (isset($validation)): ?>
+                                <?= $validation->getError('penulis'); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="tahun_terbit" class="col-sm-2 col-form-label">Tahun Terbit</label>
+                    <div class="col-sm-10">
+                        <input type="number"
+                            class="form-control <?= (isset($validation) && $validation->hasError('tahun_terbit')) ? 'is-invalid' : ''; ?>"
+                            id="tahun_terbit" name="tahun_terbit" value="<?= old('tahun_terbit'); ?>">
+                        <div class="invalid-feedback">
+                            <?php if (isset($validation)): ?>
+                                <?= $validation->getError('tahun_terbit'); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
+                    <div class="col-sm-10">
+                        <input type="file"
+                            class="form-control <?= (isset($validation) && $validation->hasError('sampul')) ? 'is-invalid' : ''; ?>"
+                            id="sampul" name="sampul">
+                        <div class="invalid-feedback">
+                            <?php if (isset($validation)): ?>
+                                <?= $validation->getError('sampul'); ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <div class="col-sm-10 ">
+                        <button type="submit" class="btn btn-primary">Insert Data</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
